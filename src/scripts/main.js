98cd31e085a25c987bb2a984e86406aee7312aec
Piotr Słobiński <piotrus@MacBook-Air-Piotr.local>
2018-12-06 12:50:53 +0100

    let

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
+// let version
 
+
+function showLength (arr){
+  if (arr.length > 5){
+    let greater = `${arr} is longer then 5`;
+  }else{
+    let smaller =`${arr} is smaller then 5`;
+    console.log(greater);
+    console.log(smaller);
+  }
+}
+showLength([1,2,3]);
+
+// var version
+
+function showLength (arr){
+  if (arr.length > 5){
+    var greater = `${arr} is longer then 5`;
+  }else{
+    var smaller =`${arr} is smaller then 5`;
+    console.log(greater);
+    console.log(smaller);
+  }
+}
+showLength([1,2,3]);
