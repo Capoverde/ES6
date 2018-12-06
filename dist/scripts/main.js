68a9ef91f7754611c6431f4d1a029af00af1e0d9
Piotr Słobiński <piotrus@MacBook-Air-Piotr.local>
2018-12-06 22:02:58 +0100

    Object initialization

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,35 @@
+'use strict';
+
 //  ------------- OBJECTS ----------------- //
 
 // -- Object initialization -- //
-"use strict";
+
+
+//  Objects literals
+// ES5
+
+// function createUser(firstname, lastname){
+//   let fullname = `${firstname} ${lastname}`;
+//   return {
+//     firstname: firstname,
+//     lastname: lastname,
+//     fullname: fullname
+//   }
+// }
+// let user = createUser('Jan', 'Kowalski')
+// console.log(user.firstname, user.lastname, user.fullname);
+// output >> Jan | Kowalski | Jan Kowalski
+
+// ES6
+
+function createUser(firstname, lastname) {
+  var fullname = firstname + ' ' + lastname;
+  return {
+    firstname: firstname,
+    lastname: lastname,
+    fullname: fullname
+  };
+}
+var user = createUser('Jan', 'Kowalski');
+console.log(user.firstname, user.lastname, user.fullname);
+// output >> Jan | Kowalski | Jan Kowalski
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,31 @@
 
 
 
+//  Objects literals
+// ES5
+
+// function createUser(firstname, lastname){
+//   let fullname = `${firstname} ${lastname}`;
+//   return {
+//     firstname: firstname,
+//     lastname: lastname,
+//     fullname: fullname
+//   }
+// }
+// let user = createUser('Jan', 'Kowalski')
+// console.log(user.firstname, user.lastname, user.fullname);
+// output >> Jan | Kowalski | Jan Kowalski
+
+// ES6
+
+function createUser(firstname, lastname){
+  let fullname = `${firstname} ${lastname}`;
+  return {
+    firstname,
+    lastname,
+    fullname
+  }
+}
+let user = createUser('Jan', 'Kowalski')
+console.log(user.firstname, user.lastname, user.fullname);
+// output >> Jan | Kowalski | Jan Kowalski
