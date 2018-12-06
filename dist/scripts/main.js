edd58eb574f006c587309f0efa050538753cde89
Piotr Słobiński <piotrus@MacBook-Air-Piotr.local>
2018-12-06 13:01:22 +0100

    let infor loops

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// ------- let keyword -------- //
+
 // let version
 
 
@@ -26,3 +28,38 @@ function showLength(arr) {
   }
 }
 showLength([1, 2, 3]);
+
+// ------ let in for loops ------ //
+
+// var version
+
+var func = [];
+
+for (i = 0; i < 5; i++) {
+  func.push(function () {
+    console.log(i);
+  });
+}
+
+func[0]();
+func[1]();
+func[4]();
+// output >> 5,5,5
+
+// let version
+var func = [];
+
+var _loop = function _loop(_i) {
+  func.push(function () {
+    console.log(_i);
+  });
+};
+
+for (var _i = 0; _i < 5; _i++) {
+  _loop(_i);
+}
+
+func[0]();
+func[1]();
+func[4]();
+// output >> 0,1,4
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+// ------- let keyword -------- //
+
 // let version
 
 
@@ -24,3 +26,30 @@ function showLength (arr){
   }
 }
 showLength([1,2,3]);
+
+// ------ let in for loops ------ //
+
+// var version
+
+var func = []
+
+for (i = 0; i < 5; i++) {
+  func.push(function(){console.log(i)});
+}
+
+func[0]();
+func[1]();
+func[4]();
+// output >> 5,5,5
+
+// let version
+var func = []
+
+for (let i = 0; i < 5; i++) {
+  func.push(function(){console.log(i)});
+}
+
+func[0]();
+func[1]();
+func[4]();
+// output >> 0,1,4
