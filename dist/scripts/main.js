f0a89b8555bcc199204f3c52e963c6b79a050290
Piotr Słobiński <piotrus@MacBook-Air-Piotr.local>
2018-12-07 18:13:15 +0100

    ES6 Classes

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,42 +1,75 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // ----------------------- Classes ----------------------- //
 
 // Constructor functions and Inheritance
 
-function Car(carspec) {
-  this.name = carspec.name;
-  this.model = carspec.model;
-  this.description = carspec.description;
-}
+// ------------ ES5 ------------- //
 
-Car.prototype.drive = function () {
-  console.log('Driving...');
-};
+// function Car(carspec){
+//   this.name = carspec.name;
+//   this.model = carspec.model;
+//   this.description = carspec.description;
+// }
+
+// Car.prototype.drive = function(){
+//   console.log('Driving...');
+// }
 
 // let car = new Car({name: 'Ford', model: 'Galaxy', description: 'Family car'});
 
 // console.log(car.drive());//>> Driving...
 
-function Audi(carSpec) {
-  Car.call(this, carSpec);
-  this.engine = carSpec.engine;
-  this.audiDrive = function () {
-    console.log('Audi Driving...');
-  };
-}
+// function Audi(carSpec){
+//   Car.call(this, carSpec);
+//   this.engine = carSpec.engine;
+//   this.audiDrive = function(){
+//     console.log('Audi Driving...');
+//   }
+// }
 
-Audi.prototype = Object.create(Car.prototype);
+// Audi.prototype = Object.create(Car.prototype);
 
-Audi.prototype.constructor = Audi;
+// Audi.prototype.constructor = Audi;
 
-var myAudi = new Audi({
-  name: 'My Audi',
-  model: 'A6',
-  description: 'Grat German car',
-  engine: 'A313'
-});
+// const myAudi = new Audi({
+//   name: 'My Audi',
+//   model: 'A6',
+//   description: 'Grat German car',
+//   engine: 'A313'
+// });
 
-console.log(myAudi.description); // >> Great German car
+// console.log(myAudi.description); // >> Great German car
 
 // ----------- ES6 CLASSES ------------- //
+
+var Car = function () {
+  function Car(carSpec) {
+    _classCallCheck(this, Car);
+
+    this.name;
+    this.model = carSpec.model;
+    this.description = carSpec.description;
+  }
+
+  // Adding methods to class:
+
+
+  _createClass(Car, [{
+    key: 'drive',
+    value: function drive() {
+      console.log('Driving...');
+    }
+  }]);
+
+  return Car;
+}();
+
+var car = new Car({ name: 'Ford', model: 'Galaxy', description: 'family car' });
+
+console.log(car.description); // >> family car
+console.log(car.drive()); // >> Driving...
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,62 @@
 
 // Constructor functions and Inheritance
 
-function Car(carspec){
-  this.name = carspec.name;
-  this.model = carspec.model;
-  this.description = carspec.description;
-}
+// ------------ ES5 ------------- //
 
-Car.prototype.drive = function(){
-  console.log('Driving...');
-} 
+// function Car(carspec){
+//   this.name = carspec.name;
+//   this.model = carspec.model;
+//   this.description = carspec.description;
+// }
+
+// Car.prototype.drive = function(){
+//   console.log('Driving...');
+// }
 
 // let car = new Car({name: 'Ford', model: 'Galaxy', description: 'Family car'});
 
 // console.log(car.drive());//>> Driving...
 
-function Audi(carSpec){
-  Car.call(this, carSpec);
-  this.engine = carSpec.engine;
-  this.audiDrive = function(){
-    console.log('Audi Driving...');
+// function Audi(carSpec){
+//   Car.call(this, carSpec);
+//   this.engine = carSpec.engine;
+//   this.audiDrive = function(){
+//     console.log('Audi Driving...');
+//   }
+// }
+
+// Audi.prototype = Object.create(Car.prototype);
+
+// Audi.prototype.constructor = Audi;
+
+// const myAudi = new Audi({
+//   name: 'My Audi',
+//   model: 'A6',
+//   description: 'Grat German car',
+//   engine: 'A313'
+// });
+
+// console.log(myAudi.description); // >> Great German car
+
+// ----------- ES6 CLASSES ------------- //
+
+class Car{
+  constructor(carSpec){
+   this.name;
+   this.model = carSpec.model;
+   this.description = carSpec.description;
+  }
+
+  // Adding methods to class:
+  drive(){
+    console.log('Driving...');
   }
 }
 
-Audi.prototype = Object.create(Car.prototype);
+let car = new Car({name: 'Ford', model: 'Galaxy', description: 'family car'});
 
-Audi.prototype.constructor = Audi;
+console.log(car.description); // >> family car
+console.log(car.drive()); // >> Driving...
 
-const myAudi = new Audi({
-  name: 'My Audi',
-  model: 'A6',
-  description: 'Grat German car',
-  engine: 'A313'
-});
 
-console.log(myAudi.description); // >> Great German car
-
-// ----------- ES6 CLASSES ------------- //
 
