4f6c76b0caf9d8baf495698e034021d6b5b57881
Piotr Słobiński <piotrus@MacBook-Air-Piotr.local>
2018-12-07 17:46:09 +0100

    Constructor functions and Inheritance - finished

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,40 @@
+'use strict';
+
 // ----------------------- Classes ----------------------- //
 
 // Constructor functions and Inheritance
-"use strict";
+
+function Car(carspec) {
+  this.name = carspec.name;
+  this.model = carspec.model;
+  this.description = carspec.description;
+}
+
+Car.prototype.drive = function () {
+  console.log('Driving...');
+};
+
+// let car = new Car({name: 'Ford', model: 'Galaxy', description: 'Family car'});
+
+// console.log(car.drive());//>> Driving...
+
+function Audi(carSpec) {
+  Car.call(this, carSpec);
+  this.engine = carSpec.engine;
+  this.audiDrive = function () {
+    console.log('Audi Driving...');
+  };
+}
+
+Audi.prototype = Object.create(Car.prototype);
+
+Audi.prototype.constructor = Audi;
+
+var myAudi = new Audi({
+  name: 'My Audi',
+  model: 'A6',
+  description: 'Grat German car',
+  engine: 'A313'
+});
+
+console.log(myAudi.description); // >> Great German car
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,37 @@
 
 // Constructor functions and Inheritance
 
+function Car(carspec){
+  this.name = carspec.name;
+  this.model = carspec.model;
+  this.description = carspec.description;
+}
+
+Car.prototype.drive = function(){
+  console.log('Driving...');
+} 
+
+// let car = new Car({name: 'Ford', model: 'Galaxy', description: 'Family car'});
+
+// console.log(car.drive());//>> Driving...
+
+function Audi(carSpec){
+  Car.call(this, carSpec);
+  this.engine = carSpec.engine;
+  this.audiDrive = function(){
+    console.log('Audi Driving...');
+  }
+}
+
+Audi.prototype = Object.create(Car.prototype);
+
+Audi.prototype.constructor = Audi;
+
+const myAudi = new Audi({
+  name: 'My Audi',
+  model: 'A6',
+  description: 'Grat German car',
+  engine: 'A313'
+});
+
+console.log(myAudi.description); // >> Great German car
